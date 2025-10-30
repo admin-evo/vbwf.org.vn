e0d189346b82d87f5ff52e5a4f056e5398866b90
unknown <sonhoangdang0612@gmail.com>
2025-10-31 02:36:33 +0700

    feat: course page

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+import Banner from "@/components/sections/courses/Banner";
+import CommonQuestions from "@/components/sections/courses/CommonQuestions";
+import ShortInfo from "@/components/sections/courses/ShortInfo";
 
 const Courses = () => {
-  return <div>Courses</div>;
+  return (
+    <div>
+      <div className="bg-gradient-to-b from-white to-[#FFECD5]">
+        <Banner />
+      </div>
+      <div className="bg-gradient-to-b from-[#DDE8EE] to-[#FFECD5]">
+        <ShortInfo />
+      </div>
+      <div className="bg-[#F2F2F7]">
+        <CommonQuestions />
+      </div>
+    </div>
+  );
 };
 
 export default Courses;
