cfcdd70304fd268d7fed915fad07184aacc91398
Son Dang <dhson0612@gmail.com>
2025-11-06 10:40:57 +0700

    feat: empty privacy and support page

---
+++ src/app/(others)/supports/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Page = () => {
+  return (
+    <div className="md:h-[calc(100vh-96px)] h-[calc(100vh-72px)] flex items-center justify-center">
+      Support
+    </div>
+  );
+};
+
+export default Page;
